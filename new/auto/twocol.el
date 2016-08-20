d83e4bedf37e8df9b0fb27571bf49fbe68bd21e9
Saksham Sharma <saksham0808@gmail.com>
2016-08-20 14:06:27 +0530

    Add germany and iit name

--- new/auto/twocol.el
+++ new/auto/twocol.el
@@ -3,6 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "tccv"
@@ -11,6 +18,8 @@
     "fontspec"
     "fontawesome")
    (TeX-add-symbols
-    "boxwidth"))
+    "boxwidth")
+   (LaTeX-add-xcolor-definecolors
+    "myred"))
  :latex)
 
